b302339b3f03483756ffa00c7f062c97fdbe88fa
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-09-28 22:09:15 -0500

    feat(iac): añadir variables de conexión a Proxmox

---
+++ iac/variables.tf
@@ -0,0 +1,10 @@
+variable "proxmox_api_url" {
+  description = "Proxmox API URL"
+  type        = string
+}
+
+variable "proxmox_api_token" {
+  description = "Proxmox API Token"
+  type        = string
+  sensitive   = true
+}
