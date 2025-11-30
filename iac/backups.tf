f2fcd97a0184b82308a918603967d9d55d68b475
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-11-29 22:31:32 -0500

    feat(terraform): agregar contenedor backups con Debian 12, VLAN 30 y bind mount para almacenamiento

---
+++ iac/backups.tf
@@ -0,0 +1,71 @@
+resource "proxmox_virtual_environment_container" "backups" {
+  provider  = proxmox.rootpam
+  vm_id     = 725
+  node_name = "proxmox"
+
+  description = "LXC dedicado a cronjobs de backup (PostgreSQL → disco sólido /mnt/pve/backups)"
+  unprivileged = false
+
+  features {
+    nesting = true
+    keyctl  = true
+    fuse    = true
+  }
+
+  initialization {
+    hostname = "backups"
+
+    ip_config {
+      ipv4 {
+        address = "10.30.0.3/24"
+        gateway = "10.30.0.254"
+      }
+    }
+
+    user_account {
+      keys     = [file(var.ansible_key)]
+      password = "12345"
+    }
+  }
+
+  operating_system {
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+  }
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 1024
+  }
+
+  network_interface {
+    name    = "eth0"
+    bridge  = "vmbr0"
+    vlan_id = 30
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+
+  # Bind mount al disco sólido montado en el host (/mnt/pve/backups)
+  mount_point {
+    volume = "/mnt/pve/backups"
+    path   = "/mnt/solid"
+    acl    = false
+    quota  = false
+    backup = false
+  }
+
+ # startup {
+  #  order      = 5
+   # up_delay   = 20
+    #down_delay = 10
+  #}
+
+  start_on_boot = true
+}
