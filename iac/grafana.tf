e9d3f20cb859dda1795d3bdb1cce3c83ed5a9d13
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-11-29 22:23:06 -0500

    feat(terraform): actualizar grafana añadiendo rootpam, Debian 12 y nueva configuración de red

--- iac/grafana.tf
+++ iac/grafana.tf
@@ -1,40 +1,62 @@
 resource "proxmox_virtual_environment_container" "grafana" {
-  vm_id     = 702
-  node_name = "proxmox"
+  provider   = proxmox.rootpam
+  vm_id      = 708
+  node_name  = "proxmox"
 
   initialization {
     hostname = "grafana"
 
+    # eth0: public LAN
     ip_config {
       ipv4 {
-        address = "10.10.0.3/24"
-        gateway = "10.10.0.2"
+        address = "192.168.0.212/24"
+      }
+    }
+
+    # eth1: VLAN 10 private network
+    ip_config {
+      ipv4 {
+        address = "10.10.0.2/24"
+        gateway = "10.10.0.254"
       }
     }
 
     user_account {
-      keys = [var.ansible_pub_key]
+      keys     = [file(var.ansible_key)]
+      password = "12345"
     }
   }
-    
+
   operating_system {
-    type             = "alpine"
-    template_file_id = "local:vztmpl/devuan-5.0-standard_5.0_amd64.tar.gz"
+    type             = "debian"
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
   }
 
   cpu { cores = 1 }
-  memory { dedicated = 512 }
-
-  network_interface {
-    name   = "eth0"
-    bridge = "vmbr0"
-    vlan_id = 10
-  }
+  memory { dedicated = 1024 }
 
   disk {
     datastore_id = "local-lvm"
-    size         = 8
+    size         = 10
   }
-  
+
+  # network interfaces
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr0"
+  }
+
+  network_interface {
+    name    = "eth1"
+    bridge  = "vmbr0"
+    vlan_id = 10
+  }
+
   unprivileged = false
+
+  features {
+    nesting = true
+    keyctl  = true
+    fuse    = true
+  }
 }
